ef779c2519314ea369e74bb25f3e372f9471791f
Jetmir007 <jeku07001@utb.vaxjo.se>
2025-01-24 13:41:57 +0100

    Test JS

--- test2.js
+++ test2.js
@@ -3,12 +3,21 @@ function knapp(){
     if(a){
         document.getElementById("t1").style.display = "none"
         document.getElementById("hidden").style.display = "block"
+        document.getElementById("button").innerHTML = "Byt till sämre text"
         a = false
     }
     else{
         document.getElementById("t1").style.display = "block"
-        document.getElementById("hidden").style.display = "none"       
+        document.getElementById("hidden").style.display = "none"
+        document.getElementById("button").innerHTML = "Byt till bättre text"
+
         a = true
     }
-    
 }
+
+let b = 1;
+function Läggtill(){
+    document.getElementById("lägg").innerHTML += "<p>\n</p>"+b+"<h3> Hej hur mår du idag </h3>"
+    b+=1
+}
+
